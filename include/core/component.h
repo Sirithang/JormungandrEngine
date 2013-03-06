e8ad77c686ea59581aef87abe69259422d903ee0
Guillaume <saby.guillaume@gmail.com>
2013-03-06 20:39:49 +0100

    Merged component to empty

--- include/core/component.h
+++ include/core/component.h
@@ -9,13 +9,6 @@ namespace jormungandr
 {
     class Component
     {
-    private:
-        static std::vector<Component*> g_Components;
-
-        Entity      _owningEntity;
-        uint16_t    _typeGUID;
-
-    public:
-        virtual void update() = 0;
+		
     };
 }
